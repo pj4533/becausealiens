1b43ade553def515458f7f9150ba6bea3d171494
PJ Gray <pj@evertrue.com>
2013-09-02 20:23:50 -0400

    Bug fixing edge cases

--- lib/becausealiens/commands/scrape.rb
+++ lib/becausealiens/commands/scrape.rb
@@ -17,36 +17,31 @@ command :'scrape' do |c|
 	local = client.db('local')
 	aliens = local.create_collection('ufo_reports')
 
-#	Anemone.crawl("http://www.nuforc.org/webreports/ndxpost.html") do |anemone|
-	Anemone.crawl("http://www.nuforc.org/webreports/ndxp130830.html") do |anemone|
-  		anemone.on_pages_like %r{^http://www.nuforc.org/webreports/[^.]+/[^.]+.html$} do |page|
-      		say_ok page.url.to_s
-  			doc = page.doc
-  			outputData = Hash.new
-			doc.search("tbody tr").each do |v|
-				cells = v.search 'td font'
-				textToProcess = cells[0].inner_html
-
-				if /^Occurred/.match textToProcess
-					textToProcess.split('<br>').each do |textLine|
-						splitTextLine = textLine.split(':',2)
-						outputData[splitTextLine[0].strip] = splitTextLine[1].strip
-					end
-				else
-					outputData['Report'] = textToProcess.gsub("<br>", " ")
-					outputData['FleschKincaidGrade'] = Odyssey.flesch_kincaid_grade_level(outputData['Report'])
-					outputData['FleschKincaidEase'] = Odyssey.flesch_kincaid_reading_ease(outputData['Report'])
-					outputData['GunningFog'] = Odyssey.gunning_fog(outputData['Report'])
-					outputData['ColemanLiau'] = Odyssey.coleman_liau(outputData['Report'])
-					outputData['Smog'] = Odyssey.smog(outputData['Report'])
-					outputData['Ari'] = Odyssey.ari(outputData['Report'])
+	Anemone.crawl("http://www.nuforc.org/webreports/ndxpost.html") do |anemone|
+		anemone.on_pages_like %r{^http://www.nuforc.org/webreports/[^.]+/S[^.]+.html$} do |page|
+			say_ok page.url.to_s
+			doc = page.doc
+			outputData = Hash.new			
+			topText = doc.search("tbody tr")[0].search('td font')[0].inner_html
+			topText.split('<br>').each do |textLine|
+				splitTextLine = textLine.split(':',2)
+				if splitTextLine.count == 2
+					outputData[splitTextLine[0].strip] = splitTextLine[1].strip
 				end
 			end
 
+			bottomText = doc.search("tbody tr")[1].search('td font')[0].inner_html
+			outputData['Report'] = bottomText.gsub("<br>", " ")
+			outputData['FleschKincaidGrade'] = Odyssey.flesch_kincaid_grade_level(outputData['Report'])
+			outputData['FleschKincaidEase'] = Odyssey.flesch_kincaid_reading_ease(outputData['Report'])
+			outputData['GunningFog'] = Odyssey.gunning_fog(outputData['Report'])
+			outputData['ColemanLiau'] = Odyssey.coleman_liau(outputData['Report'])
+			outputData['Smog'] = Odyssey.smog(outputData['Report'])
+			outputData['Ari'] = Odyssey.ari(outputData['Report'])
+
 			# save data to a datasource here
 			aliens.insert(outputData)
-			puts outputData
-  		end
+		end
 	end
 
 
